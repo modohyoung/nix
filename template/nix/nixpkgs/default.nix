45b544149f032565dd4748ad4b5e73d68750f0bb
Doh Young Mo <modohyoung@gmail.com>
2018-08-17 07:18:31 +0900

    sha256 is used now.

--- template/nix/nixpkgs/default.nix
+++ template/nix/nixpkgs/default.nix
@@ -1,6 +1,8 @@
 let
   spec = builtins.fromJSON (builtins.readFile ./nixpkgs-src.json);
   rev = spec.rev;
-  url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
 in
-  builtins.fetchTarball url
+  builtins.fetchTarball {
+    url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
+    sha256=spec.sha256;
+  }
